2aa646a8643c1e1d66448f891a02cb5f0077dc28
d2jvkpn@noreply.local <d2jvkpn@noreplay.local>
2024-06-04 06:43:39 +0800

    ...

--- deployments/docker_build.sh
+++ deployments/docker_build.sh
@@ -11,6 +11,11 @@ app_name=$(yq .app_name project.yaml)
 app_version=$(yq .version project.yaml)
 image_name=$(yq .image_name project.yaml)
 
+if [[ "$app_name" == "null" || "$image_name" == "null" ]]; then
+    >&2 echo "neither app_name or image_name is unset in project.yaml"
+    exit 1
+fi
+
 tag=${git_branch}-$(yq .version project.yaml)
 tag=${DOCKER_Tag:-$tag}
 image=$image_name:$tag
--- deployments/docker_deploy.sh
+++ deployments/docker_deploy.sh
@@ -2,7 +2,7 @@
 set -eu -o pipefail # -x
 _wd=$(pwd); _path=$(dirname $0 | xargs -i readlink -f {})
 
-export IMAGE_Tag=$1 APP_Tag=$2 HTTP_Port=$3
+export IMAGE_Tag=$1 HTTP_Port=$2
 export IMAGE_Name=$(yq .image_name project.yaml)
 export APP_Name=$(yq .app_name project.yaml)
 export USER_UID=$(id -u) USER_GID=$(id -g)
@@ -11,12 +11,15 @@ container=${APP_Name}_${APP_Tag}
 
 envsubst < ${_path}/docker_deploy.yaml > docker-compose.yaml
 
+exit 0
+
 # docker-compose pull
 [ ! -z "$(docker ps --all --quiet --filter name=$container)" ] &&
   docker rm -f $container
 # 'docker-compose down' removes running containers only, not stopped containers
 
-USER_UID=$USER_UID USER_GID=$USER_GID docker-compose up -d
+# USER_UID=$USER_UID USER_GID=$USER_GID docker-compose up -d
+docker-compose up -d
 
 exit 0
 
